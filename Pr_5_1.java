2aa88e8970d7f8eca5bc1594a8fed3da5f33bd19
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2020-09-30 20:03:28 +0300

    Add files via upload

---
+++ Pr_5_1.java
@@ -0,0 +1,24 @@
+package ru.mirea.Lab5.Pr_5_1;
+
+public class Pr_5_1 {
+
+    public static String recursion(int n) {
+        int sum = 0;
+        int j = 0;
+
+        if (n == 1) {
+            System.out.print("1");
+        } else {
+            for (int i = 1; sum < n; i++) {
+                sum += i;
+                j = i;
+            }
+            System.out.print(recursion(--n) + " " + j);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        recursion(6);
+    }
+}
